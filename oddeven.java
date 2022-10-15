cbcf8b8d767f86ef75eabd4831d4c99e536f6ce7
Satyam Solanki <92463204+satyam2k3@users.noreply.github.com>
2022-10-16 00:37:59 +0530

    create OddEven.Java

---
+++ oddeven.java
@@ -0,0 +1,7 @@
+	public static void main(String[] args) {
+		int[] arr = { 1, 2, 2, 3, 3, 4, 5, 6 };
+		for (int i = 0; i < arr.length; i++) {
+			for (int idx = i; idx < arr.length; idx++) {
+
+			}
+		}
